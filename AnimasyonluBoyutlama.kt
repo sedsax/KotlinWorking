5fb89994d25c56e3040915260bb0263d5f313d52
Sedanur Savaş <44877350+sedsax@users.noreply.github.com>
2022-09-18 11:24:03 +0300

    Update AnimasyonluBoyutlama.kt

--- AnimasyonluBoyutlama.kt
+++ AnimasyonluBoyutlama.kt
@@ -3,6 +3,9 @@ fun Animasyon() {
     var sizeState by remember { mutableStateOf(150.dp)}
     val size by animateDpAsState(
         targetValue = sizeState,
+   /*     spring(
+            Spring.DampingRatioMediumBouncy
+        )*/
         tween(
             durationMillis = 3000,
             delayMillis = 300,
