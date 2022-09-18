ad39737f3582d2e8ffa0f2740c4bf397f6ccb4ef
Sedanur Savaş <44877350+sedsax@users.noreply.github.com>
2022-09-18 11:15:23 +0300

    Create AnimasyonluBoyutlama.kt

---
+++ AnimasyonluBoyutlama.kt
@@ -0,0 +1,29 @@
+@Composable
+fun Animasyon() {
+    var sizeState by remember { mutableStateOf(150.dp)}
+    val size by animateDpAsState(
+        targetValue = sizeState,
+        tween(
+            durationMillis = 3000,
+            delayMillis = 300,
+            easing = LinearOutSlowInEasing
+        )
+    )
+   Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+       Box(modifier = Modifier
+           .size(size)
+           .background(Color.Green),
+           contentAlignment = Alignment.Center) {
+           Button(onClick = {
+               sizeState += 50.dp
+           }) {
+               Text(text = "Büyüt")
+           }
+       }
+       Button(onClick = {
+           sizeState -= 50.dp
+       }) {
+           Text(text = "Küçült")
+       }
+   }
+}
